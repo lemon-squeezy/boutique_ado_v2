cc5380ec3eab3ffd17e3f1d978a505ee1274a911
lemon-squeezy <AdrianLaub@aol.com>
2021-01-17 14:05:13 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IAbVAAiJ32aQkWKYfFvBSCMSzkch3ctfILmtfGwQxVFdxabJ1GS6QXAooys50MZFpJhkNYUOiFZzYinHQIb9ZIR0080wsdwq3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
